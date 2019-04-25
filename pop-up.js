6e27fbba569877fbe6ca834f7e84a21fec2e4343
Alec Ostheimer <alecostheimer94@gmail.com>
2019-04-25 11:28:22 -0600

    working product. Needs more styling and other tweaks.

---
+++ pop-up.js
@@ -0,0 +1,16 @@
+const timeOut = 1000;
+
+window.onload = function(){
+    let popUp = document.getElementById('pop-up');
+    setTimeout(function(){
+        popUp.style.display = 'block';
+    }, timeOut);
+}
+
+window.onclick = function(event) {
+    let popUp = document.getElementById('pop-up');
+    let cancel = document.getElementById('cancel');
+    if (event.target == popUp || event.target == cancel) {
+      popUp.style.display = "none";
+    }
+  }
